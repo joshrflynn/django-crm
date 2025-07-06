e98e12dd685c1aac2fe396640c61bcbd66fa9575
Joshua Flynn <88634411+joshrflynn@users.noreply.github.com>
2025-07-06 18:02:55 -0500

    sign in and out changes (#1)

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
 ]
